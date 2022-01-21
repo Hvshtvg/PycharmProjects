8cf3ffeb4ba2d250c188e1f7855c442c28345b55
Hvshtvg <p4tr1ku@gmail.com>
2022-01-21 23:29:38 +0100

    Tracking hand

--- Test/MotionCaptureTest.py
+++ Test/MotionCaptureTest.py
@@ -2,9 +2,28 @@ import cv2
 import mediapipe as mp
 import time
 
+# Volba web kamery - 0 = integrovaná, 1 = externí
 cam = cv2.VideoCapture(1)
 
+mpHands = mp.solutions.hands
+hands = mpHands.Hands() #ctrl+klik k více informacím o parametrech
+mpDraw = mp.solutions.drawing_utils
+pTime = 0 #previous time
+cTime = 0 #current time
+
 while True:
-    success, img = cam.read()
-    cv2.imshow("Webcam image", img)
-    cv2. waitKey(1)
+    success, img = cam.read()  #aktivace kamery
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                print(id,lm)
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    cv2.imshow("Webcam image", img) #okno s obrazem z kamery
+    cv2.waitKey(1)
