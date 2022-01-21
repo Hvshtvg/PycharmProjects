d95f6d1143c2161b9d5a13f6ca0d84197805c486
Hvshtvg <p4tr1ku@gmail.com>
2022-01-21 23:53:54 +0100

    Tracking hand

--- Test/MotionCaptureTest.py
+++ Test/MotionCaptureTest.py
@@ -12,18 +12,24 @@ pTime = 0 #previous time
 cTime = 0 #current time
 
 while True:
-    success, img = cam.read()  #aktivace kamery
+    success, img = cam.read()  #turn on camera
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             for id, lm in enumerate(handLms.landmark):
-                print(id,lm)
+                #print(id,lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(cx, cy)
+                if id == 0:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
+    #FPS counter top left
     cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-    cv2.imshow("Webcam image", img) #okno s obrazem z kamery
+    cv2.imshow("Webcam image", img) # camera window
     cv2.waitKey(1)
